d002b1a174c68803a8094ba77df8f6b37e0d620b
amars29 <amars@geekyants.com>
2020-11-18 11:23:33 +0530

    data rendering in the StockList

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import stocks from "./defaultValue";
 
-export const StockContext = React.createContext({});
+export const StockContext = React.createContext(stocks);
 
 function App() {
+  const stocksCollection = useState(stocks);
+
   return (
     <div className="bg-light">
       <StockContext.Provider value={stocks}>
