cd73d80b622ad414eb25a03703c15fe0593fec75
amars29 <amars@geekyants.com>
2020-11-18 17:02:31 +0530

    ChartPlugin intiliased

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,14 @@ import {
 import SkeletonPlugin from "./plugins/SkeletonPlugin";
 import AuthPlugin from "./plugins/AuthPlugin";
 import ContentPlugin from "./plugins/ContentPlugin";
+import ChartPlugin from "./plugins/ChartPlugin";
 
 export const StockContext = React.createContext(stocks);
 
 const pluginStore: PluginStore = createPluginStore();
 pluginStore.install(new RendererPlugin());
 pluginStore.install(new AuthPlugin());
+pluginStore.install(new ChartPlugin());
 pluginStore.install(new ContentPlugin());
 pluginStore.install(new SkeletonPlugin());
 
---
+++ src/plugins/ChartPlugin.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { IPlugin, PluginStore } from "react-pluggable";
+
+class ChartPlugin implements IPlugin {
+  namespace = "Chart";
+  pluginStore!: PluginStore;
+
+  private installedChart = new Map<string, React.Component>();
+  private selectedChart!: React.Component;
+
+  addChart = (name: string, component: React.Component) => {
+    this.installedChart.set(name, component);
+  };
+
+  getPluginName(): string {
+    return `${this.namespace}@0.0.1`;
+  }
+  getDependencies(): string[] {
+    return [];
+  }
+  init(pluginStore: PluginStore): void {
+    this.pluginStore = pluginStore;
+  }
+  activate(): void {
+    this.pluginStore.addFunction(
+      `${this.namespace}.addChart`,
+      this.addChart.bind(this)
+    );
+  }
+  deactivate(): void {
+    this.pluginStore.removeFunction(`${this.namespace}.addChart`);
+  }
+}
+
+export default ChartPlugin;
