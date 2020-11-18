c159bf07bce8285f6c04bf8c5c8d2eb2642bcb03
amars29 <amars@geekyants.com>
2020-11-18 16:41:36 +0530

    AuthPlugin and ContentPlugin initialised

--- src/plugins/ContentPlugin.tsx
+++ src/plugins/ContentPlugin.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { IPlugin, PluginStore } from "react-pluggable";
 
-class PanelPlugin implements IPlugin {
-  namespace = "Panel";
+class ContentPlugin implements IPlugin {
+  namespace = "Content";
   pluginStore!: PluginStore;
-
   getPluginName(): string {
     return `${this.namespace}@0.0.1`;
   }
-
   getDependencies(): string[] {
     return [];
   }
-
   init(pluginStore: PluginStore): void {
     this.pluginStore = pluginStore;
   }
-
-  activate(): void {}
-  deactivate(): void {}
+  activate(): void {
+    //
+  }
+  deactivate(): void {
+    //
+  }
 }
 
-export default PanelPlugin;
+export default ContentPlugin;
